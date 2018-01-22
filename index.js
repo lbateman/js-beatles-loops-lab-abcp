0f06632e48c9561dbe89ff4a4f0c07104d022c08
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-22 23:26:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  
+  for (i = 0; i < musicians.length; i++){
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  
+  return newArray;
+}
